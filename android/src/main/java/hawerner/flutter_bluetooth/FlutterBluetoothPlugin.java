41001b1fb0ce3355e69bbe99b3a57ad528874f31
hwrnr <hawerner.hawerner@gmail.com>
2019-03-30 17:36:34 +0100

    Initial commit

---
+++ android/src/main/java/hawerner/flutter_bluetooth/FlutterBluetoothPlugin.java
@@ -0,0 +1,25 @@
+package hawerner.flutter_bluetooth;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FlutterBluetoothPlugin */
+public class FlutterBluetoothPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_bluetooth");
+    channel.setMethodCallHandler(new FlutterBluetoothPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ test/flutter_bluetooth_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_bluetooth/flutter_bluetooth.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('flutter_bluetooth');
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+
+  test('getPlatformVersion', () async {
+    expect(await FlutterBluetooth.platformVersion, '42');
+  });
+}
